c57a2941915876ef853833002753c8eeb63157c9
Akshay Gadekar <akshay.gadekar@infosys.com>
2019-12-04 16:38:26 +0530

    Task#2: Code changes to make app working

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,25 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { StocksComponent } from './stocks.component';
+import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { StoreModule } from '@ngrx/store';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatButtonModule, MatNativeDateModule } from '@angular/material';
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
   let fixture: ComponentFixture<StocksComponent>;
+  let facadeService: PriceQueryFacade;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
+      imports: [
+        NoopAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        SharedUiChartModule,
+        StoreModule.forRoot({})
+      ],
+      providers: [PriceQueryFacade],
+      declarations: [StocksComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StocksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    facadeService = TestBed.get(PriceQueryFacade);
+    component.ngOnInit();
   });
 
-  it('should create', () => {
+  it('should create stocks component', () => {
     expect(component).toBeTruthy();
+    expect(facadeService instanceof PriceQueryFacade).toBeTruthy();
   });
+
+  it('ngOnInit()', () => {
+    component.ngOnInit();
+    expect(typeof component.subscription).toEqual('object');
+  });
+
+  it('ngOnDestroy()', () => {
+    component.ngOnDestroy();
+    expect(typeof component.subscription).toEqual('object');
+  });
+
 });
