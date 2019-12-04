dfd3db088d1ad7af0a19c4cbfea8656f32891cf4
Akshay Gadekar <akshay.gadekar@infosys.com>
2019-12-04 18:04:55 +0530

    Task#3: Added material date-picker component and fixed other issues

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,25 +1,142 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { StocksComponent } from './stocks.component';
+import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { StoreModule } from '@ngrx/store';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatButtonModule, MatNativeDateModule } from '@angular/material';
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
   let fixture: ComponentFixture<StocksComponent>;
+  let facadeService: PriceQueryFacade;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
+      imports: [
+        NoopAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        SharedUiChartModule,
+        StoreModule.forRoot({})
+      ],
+      providers: [PriceQueryFacade],
+      declarations: [StocksComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StocksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    facadeService = TestBed.get(PriceQueryFacade);
+    component.ngOnInit();
   });
 
-  it('should create', () => {
+  it('should create stocks component', () => {
     expect(component).toBeTruthy();
+    expect(facadeService instanceof PriceQueryFacade).toBeTruthy();
   });
+
+  describe('fetchQuote()', () => {
+    let priceQuerySpy;
+
+    beforeEach(() => {
+      priceQuerySpy = jest.spyOn(facadeService, 'fetchQuote');
+    });
+
+    it('should not call fetchQuote function when stockPickerForm is invalid', () => {
+      component.fetchQuote();
+
+      expect(component.stockPickerForm.valid).toBeFalsy();
+      expect(priceQuerySpy).not.toBeCalled();
+    });
+
+    it('should call fetchQuote function when stockPickerForm is valid', () => {
+      component.stockPickerForm.controls['symbol'].setValue('FB');
+      component.stockPickerForm.controls['filterFromdate'].setValue(new Date('11/12/2019'));
+      component.stockPickerForm.controls['filterTodate'].setValue(new Date('11/14/2019'));
+
+      component.fetchQuote();
+
+      expect(component.stockPickerForm.valid).toBeTruthy();
+      expect(priceQuerySpy).toBeCalled();
+    });
+
+  });
+
+  describe('StocksComponent()', () => {
+
+    it('should return period as 1d when todate and fromdate difference is less than or equals to 1 day', () => {
+      const todate = new Date('11/14/2019');
+      const fromdate = new Date('11/14/2019');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('1d');
+    });
+
+    it('should return period as 5d when todate and fromdate difference is less than or equals to 5 days', () => {
+      const todate = new Date('11/14/2019');
+      const fromdate = new Date('11/12/2019');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('5d');
+    });
+
+    it('should return period as 1m when todate and fromdate difference is less than or equals to 30 days', () => {
+      const todate = new Date('11/24/2019');
+      const fromdate = new Date('11/09/2019');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('1m');
+    });
+
+    it('should return period as 6m when todate and fromdate difference is less than or equals to 180 days', () => {
+      const todate = new Date('11/14/2019');
+      const fromdate = new Date('10/09/2019');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('6m');
+    });
+
+    it('should return period as 1y when todate and fromdate difference is less than or equals to 365 days', () => {
+      const todate = new Date('11/14/2019');
+      const fromdate = new Date('11/14/2018');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('1y');
+    });
+
+    it('should return period as 2y when todate and fromdate difference is less than or equals to 730 days', () => {
+      const todate = new Date('11/14/2019');
+      const fromdate = new Date('03/09/2018');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('2y');
+    });
+
+    it('should return period as max when todate and fromdate difference is greater than 730 days', () => {
+      const todate = new Date('11/14/2019');
+      const fromdate = new Date('11/09/2015');
+
+      const result = component.calculateDateDiff(todate, fromdate);
+
+      expect(result).toEqual('max');
+    });
+
+  });
+
 });
--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { STOCK_CONST } from './stocks.constant';
 
 @Component({
   selector: 'coding-challenge-stocks',
@@ -10,34 +11,82 @@ import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-que
 export class StocksComponent implements OnInit {
   stockPickerForm: FormGroup;
   symbol: string;
-  period: string;
+  today = new Date();
+  filterFromdate: string;
+  filterTodate: string;
 
   quotes$ = this.priceQuery.priceQueries$;
-
-  timePeriods = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      filterFromdate: [null, Validators.required],
+      filterTodate: [null, Validators.required],
+    }, {
+      validator: this.toDateValidator('filterFromdate', 'filterTodate')
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  /**
+   * The date-pickers should not allow selection of dates after the current day.
+   * "to" cannot be before "from" (selecting an invalid range should make both dates the same value)
+   */
+  toDateValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const fromDate = formGroup.controls[controlName];
+      const toDate = formGroup.controls[matchingControlName];
+      if (toDate.errors && toDate.errors.matDatepickerMin) {
+        toDate.setValue(fromDate.value);
+      }
+    }
+  }
+
+  /**
+   * This function is called on click of submit button
+   */
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
+      const { symbol, filterFromdate, filterTodate } = this.stockPickerForm.value;
+      const period = this.calculateDateDiff(filterTodate, filterFromdate);
       this.priceQuery.fetchQuote(symbol, period);
     }
   }
+
+  /**
+   * This function calculate diff between from date and to date and returns period as : '1d/5d/1m/6m/1y/yty/2y/max 
+  */
+  calculateDateDiff(toDate: Date, fromDate: Date) {
+    const toDateValue = toDate.valueOf();
+    const fromDateValue = fromDate.valueOf();
+    const days = (toDateValue !== fromDateValue) ? Math.round(Math.abs((toDateValue - fromDateValue) / 86400000)) : 1;
+    let period;
+    switch (true) {
+      case (days <= 1):
+        period = STOCK_CONST.PERIOD['1D'];
+        break;
+      case (days <= 5):
+        period = STOCK_CONST.PERIOD['5D'];
+        break;
+      case (days <= 30):
+        period = STOCK_CONST.PERIOD['1M'];
+        break;
+      case (days <= 180):
+        period = STOCK_CONST.PERIOD['6M'];
+        break;
+      case (days <= 365):
+        period = STOCK_CONST.PERIOD['1Y'];
+        break;
+      case (days <= 730):
+        period = STOCK_CONST.PERIOD['2Y'];
+        break;
+      case (days > 730):
+        period = STOCK_CONST.PERIOD['MAX'];
+        break;
+    }
+    return period;
+
+  }
+
 }
